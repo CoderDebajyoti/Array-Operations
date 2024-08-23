bbdc7af1ab01210937d020f45933a2b6524d7866
Debajyoti Das <136669104+CoderDebajyoti@users.noreply.github.com>
2024-08-23 20:17:08 +0530

    Update deletion in array.c

--- deletion in array.c
+++ deletion in array.c
@@ -4,6 +4,14 @@ int main()
 {
     int arr[8] = {78, 98, 56, 65, 33, 99, 44, 88}, i, pos;
 
+    // Display the previous array
+    printf("The current array is: ");
+    for (i = 0; i < 8; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
     // Get position input from user
     printf("Enter the index position of the array that you want to delete the item (0-7): ");
     scanf("%d", &pos);
@@ -14,14 +22,6 @@ int main()
         printf("Error: Invalid position! Please enter a value between 0 to 7.\n");
         return 1;  // Exit the program with an error code
     }
-    
-    // Display the previous array
-    printf("The previous array is: ");
-    for (i = 0; i < 8; i++) 
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
     
     // Logic for deletion
     // Shift elements to the left to remove the specified element
